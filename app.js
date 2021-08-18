781f980e8c0bf783fde5b62c15bb66932db16574
Khurshida-Meem <meemrumi59@gmail.com>
2021-08-18 14:38:28 +0600

    mached pin

--- app.js
+++ app.js
@@ -13,7 +13,40 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+function verifyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedPin = document.getElementById('typed-numbers').value;
+    const success = document.getElementById('success');
+    const failed = document.getElementById('failed');
 
-document.getElementById('generate-pin').addEventListener('click',function () {
+    if(pin == typedPin){
+        success.style.display = 'block';
+        failed.style.display = 'none';
+    }
+    else{
+        success.style.display = 'none';
+        failed.style.display = 'block';
+    }
+}
+
+document.getElementById('generate-pin').addEventListener('click', function () {
     generatePin();
+})
+document.getElementById('reset').addEventListener('click', function () {
+    document.getElementById('display-pin').value = '';
+})
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
 })
